0e00c9155291e0d7d9c637fd399d0530588fdc25
Toshiyuki Suzumura <suz@smoche.info>
2020-02-17 13:37:34 +0900

    ios: HTMLファイルまでは表示された

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import {SafeAreaView, StatusBar, View, Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
-import path from 'react-native-path';
 
-const assetsPrefix = Platform.OS === 'android' ? 'file:///android_asset' : '';
 const styles = {
   view: {
     flex: 1,
@@ -21,7 +19,9 @@ const styles = {
 export default class App extends React.Component {
   constructor(prop) {
     super(prop);
-    this.indexHtml = path.resolve(assetsPrefix, 'view1/index.html');
+    this.indexHtml = (Platform.OS === 'android')
+      ? { uri: 'file:///android_asset/view1/index.html' }
+      : require('./webViewAssets/assets/view1/index.html');
   }
   render() {
     const viewConfig = {
@@ -42,7 +42,7 @@ export default class App extends React.Component {
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.view}>
           <View style={styles.header} />
-          <WebView source={{uri: this.indexHtml}} {...viewConfig} />
+          <WebView source={this.indexHtml} {...viewConfig} />
           <WebView source={{html: '<h1>Hello World</h1>'}} {...viewConfig} />
           <View style={styles.footer} />
         </SafeAreaView>
