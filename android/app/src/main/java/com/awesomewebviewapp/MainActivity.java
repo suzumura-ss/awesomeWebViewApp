5b0621fdc4f3d8decf886eba3a654b26c9c57203
Toshiyuki Suzumura <suz@smoche.info>
2020-02-17 10:48:45 +0900

    enable chrome-inspector for webView (android)

--- android/app/src/main/java/com/awesomewebviewapp/MainActivity.java
+++ android/app/src/main/java/com/awesomewebviewapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.awesomewebviewapp;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +14,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "awesomeWebViewApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      if (BuildConfig.DEBUG) {
+          WebView.setWebContentsDebuggingEnabled(true);
+      }
   }
 }
--- webViewAssets/src/view-1/index.js
+++ webViewAssets/src/view-1/index.js
@@ -2,3 +2,4 @@ import '../css/style.scss'
 const h2 = document.createElement('h2')
 h2.innerText = 'Hello H2'
 document.getElementById('app').appendChild(h2)
+console.log(h2)
